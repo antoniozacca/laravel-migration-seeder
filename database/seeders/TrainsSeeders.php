702dd5fa306fc2266f8ec463c0785f2389f6ceab
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-10 18:52:00 +0200

    error

--- database/seeders/TrainsSeeders.php
+++ database/seeders/TrainsSeeders.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,17 @@ class TrainsSeeders extends Seeder
     public function run(): void
     {
         $train= new Train();
+
+        $train->company = 'treniseeder';
+        $train->departure_station = 'Trento';
+        $train->arrival_station = 'Firenze';
+        $train->departure_time = '2024-08-12 12:34:00';
+        $train->arrival_time = '2024-08-12 16:34:00';
+        $train->train_code = 'TF005';
+        $train->cars_number = 15;
+        $train->on_time = 1;
+        $train->is_cancelled = 0;
+
         $train->save();
     }
 }
